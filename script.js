13929851dadfd848ab509976054141ad7ca9e104
Dylan Wickwire <dlwicky@hotmail.com>
2025-09-11 18:25:08 -0500

    added 2 more pictures as placeholders

--- script.js
+++ script.js
@@ -15,26 +15,29 @@ let currentImage = "";
 //images for second card variable
 let currentImage2 = "";
 
-
-//variable that sets the id when chosen for each monster in the dropdown
+//Drop down menus for each player
 var selectOne = document.getElementById("bossSelectOne");
 var selectTwo = document.getElementById("bossSelectTwo");
 console.log(selectOne);
-//setting my boss id's and images for each monster which were split on the comma 
+//setting my boss id's and images for each monster which were split on the comma
 function createOptions() {
-  //creates Array
+  //creates Array/list of all bosses
   var bosses = Object.keys(bossMap);
   //Loops
   bosses.forEach((boss) => {
+    //creates the choices for each drop down menu
     var option = document.createElement("option");
     var option2 = document.createElement("option");
+    //sets the text for each boss on the drop down 
     option.textContent = boss;
     option2.textContent = boss;
+    //links the picture and I to each boss so when you pick a boss it knows which
     option.value = bossMap[boss] + "," + boss.replaceAll(" ", "") + ".png";
     option2.value = bossMap[boss] + "," + boss.replaceAll(" ", "") + ".png";
-    //Creates html element from js
+    //Creates html element from js of each boss card so they show up in HTML
     selectOne.appendChild(option);
     selectTwo.appendChild(option2);
+    //changes all spaces to no spaces for the png in the image folder
     console.log(boss.replaceAll(" ", "") + ".png");
   });
 }
@@ -53,13 +56,17 @@ function updateBossCards() {
     setText("bossTwoExamine", bossExamine);
   }
 }
-//drop down menu one
+//drop down menu one function when player chooses new boss from menu
 selectOne.addEventListener("change", function () {
   player = 1;
+  //cuts the string in 2 places (at the comma)[0] means it grabs the id
   var id = selectOne.value.split(",")[0];
+  //adds the second half of the split and adds /images in front of it to get the full file path
   var image = "images/" + selectOne.value.split(",")[1];
   console.log(image);
+  //function that pulls data from the API
   getMonster(id);
+  //sets the picture to the screen with matching ID
   setImageURL("img1", image);
 });
 //drop down menu two
@@ -71,13 +78,13 @@ selectTwo.addEventListener("change", function () {
   getMonster(id);
   setImageURL("img2", image);
 });
-//pulls the id for monster selected 
+//pulls the id for monster selected
 function getMonster(monsterId) {
   const requestOptions = {
     method: "GET",
     redirect: "follow",
   };
-// api data pulling 
+  // api data pulling
   fetch(
     "https://corsproxy.io/?url=https://api.gearscape.net/api/monster/id/" +
       monsterId,
@@ -101,27 +108,27 @@ function getMonster(monsterId) {
     .catch((error) => console.error(error));
 }
 
-
-// AI STUFF 
+// AI STUFF
 let botReply = "";
-
-let userInput = "";
+setText("chatResponse", "");
 
 onEvent("fightButton", "click", function () {
-  userInput = getValue("userInput");
-  if (userInput) {
-    console.log(userInput);
-    setText("chatResponse", "Diving into Gilenor lore...");
-    setText("errorCode", "");
-    //sendToBot();
+  // gets the boss names from the dropdowns
+  let player1 = selectOne.options[selectOne.selectedIndex].textContent;
+  let player2 = selectTwo.options[selectTwo.selectedIndex].textContent;
+
+  if (player1 && player2) {
+    console.log("Player 1: " + player1 + " | Player 2: " + player2);
+    setText("chatResponse", "Diving into the world of Gielinor...");
+    //sendToBot(player1, player2);
   } else {
-    console.log("under else: " + userInput);
-    setText("errorCode", "You didnt pick enough bosses");
+    console.log("Boss not selected");
   }
 });
 
-function sendToBot() {
+function sendToBot(player1, player2) {
   console.log("sent to bot");
+
   async function query(data) {
     const response = await fetch(
       "https://router.huggingface.co/v1/chat/completions",
@@ -142,14 +149,12 @@ function sendToBot() {
     messages: [
       {
         role: "user",
-        content: " respond in 100 words or less on which Oldschool Runescape boss would win 1 on 1 and why out of " +player1+ " and " +player2
+        content: `Respond in 100 words or less on which Oldschool Runescape boss would win 1 on 1 and why out of ${player1} and ${player2}.`,
       },
     ],
     model: "deepseek-ai/DeepSeek-V3.1:fireworks-ai",
   }).then((response) => {
-    let botReply = response.choices[0].message.content;
+    botReply = response.choices[0].message.content;
     setText("chatResponse", botReply);
   });
 }
-
-
